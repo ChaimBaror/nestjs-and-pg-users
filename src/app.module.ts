ee94547417dff3580903c27946c40ce510ac3095
myusername <myemail>
2020-10-31 23:45:36 +0200

    new api

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,13 +11,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url:'postgres://svgdxyou:Y_LYIADsPdgphb2aZ_MzZFBGC3vtGYYr@kandula.db.elephantsql.com:5432/svgdxyou' ,
-      autoLoadEntities: true,
-      synchronize: true,
-      entities: [User],
-    }),
+    ,
     UsersModule],
   controllers: [AppController],
   providers: [AppService],
