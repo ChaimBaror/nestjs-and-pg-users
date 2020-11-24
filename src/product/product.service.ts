2358a3eacec38006cefdbdc65090da9632452046
myusername <myemail>
2020-11-24 15:56:03 +0200

    .

---
+++ src/product/product.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import ProductsEntity from 'src/user/entities/product.entity';
+import { ProductsDTO } from "src/user/dto/product.DTO";
+import { UpdateProductDto } from 'src/user/dto/update-product.dto.1';
+
+
+@Injectable()
+export class ProductService {
+
+    constructor(@InjectRepository(ProductsEntity) private repository: Repository<ProductsEntity>, ) { }
+    create(ProductsDTO: ProductsDTO) {
+        const { nameProduct, category, price, isActive,image,message,tineEnd } = ProductsDTO
+
+        const product = new ProductsEntity()
+        product.nameProduct = nameProduct;
+        product.category = category;
+        product.price = price;
+        product.image = image;
+        product.message = message;
+        product.tineEnd = tineEnd;
+        product.isActive = isActive;
+
+        return this.repository.save(product);
+    }
+
+    findAll() {
+        return this.repository.find();
+    }
+
+    async update(id: string, updateProDto: UpdateProductDto) {
+        return this.repository.save({ ...updateProDto, id: id });
+     
+      }
+}
