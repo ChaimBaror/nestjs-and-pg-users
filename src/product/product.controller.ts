b27ee67d5935ccf26a5e6cb9fa13c02fcf8fbaf6
myusername <myemail>
2020-12-08 15:29:07 +0200

    make it better

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Request, Body, Get, Post, Patch, Param, UsePipes, ValidationPipe, Delete } from "@nestjs/common";
+import { Controller, Request, Body, Get, Post, Patch, Param, UsePipes, ValidationPipe, Delete, Res, UseInterceptors } from "@nestjs/common";
 import { ProductsDTO } from "src/user/dto/product.DTO";
 import { ProductService } from "./product.service";
 import { UpdateProductDto } from "src/user/dto/update-product.dto.1";
+import { diskStorage } from 'multer';
+import { FileInterceptor } from "@nestjs/platform-express/multer/interceptors/file.interceptor";
+import { UploadedFile } from "@nestjs/common/decorators/http/route-params.decorator";
+import { join } from "path";
 
+
+
+export const storage = {
+  storage: diskStorage({
+    destination: './uploads/images',
+    filename: (req, file, cb) => {
+      console.log("file", file);
+      const filename = `${req['headers']['id']}.jpg`
+      cb(null, `${filename}`)
+    }
+  })
+}
 @Controller('products')
 export class ProductController {
-    
-    constructor(private ProductService : ProductService) {
-    }
-    
-    @UsePipes(ValidationPipe)
-    @Post()
-    create(@Body() productDto: ProductsDTO) {
-        console.log('productDto', productDto);
-        return this.ProductService.create(productDto);
-    }
 
-    @Get()
-    findAll(@Request() req) {
-      console.log(req.headers);
-  
-      return this.ProductService.findAll();
-    }
+  constructor(private ProductService: ProductService) {
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-      return this.ProductService.findOne(id);
-    }
+  @UsePipes(ValidationPipe)
+  @Post()
+  create(@Body() productDto: ProductsDTO) {
+    console.log('productDto', productDto);
+    return this.ProductService.create(productDto);
+  }
 
-    @Patch(':id')
+  @Get()
+  findAll(@Request() req) {
+    console.log(req.headers);
+
+    return this.ProductService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ProductService.findOne(id);
+  }
+
+  @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.ProductService.update(id, updateProductDto);
   }
@@ -36,5 +52,16 @@ export class ProductController {
   remove(@Param('id') id: string) {
     console.log("remove", id);
     return this.ProductService.remove(id);
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('image', storage))
+  uploadFile(@UploadedFile() file) {
+    console.log(file)
+    return { filename: file.filename }
+  }
+  @Get('getFile/:id')
+  getfile(@Param('id') id, @Res() res: any) {
+    return res.sendFile(join(process.cwd(), `uploads/images/${id}.jpg`))
   }
 }
